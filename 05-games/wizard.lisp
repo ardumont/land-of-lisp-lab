e833c6a256755dbfad127ad2188fdefebc2bc7e1
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-10-30 22:27:29 +0100

    walk - a side effect function that permits the walking of the player from one location to another.

--- 05-games/wizard.lisp
+++ 05-games/wizard.lisp
@@ -86,4 +86,54 @@
 (setf *location* 'living-room)
 (look)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; walk
 
+;; goal: walking in a direction through a real passage
+;; side-effect: modify the current *location* variable
+
+*nodes*
+*edges*
+
+(find 'living-room '(attic living-room garden))
+;; living-room
+(find 'test '(attic living-room garden))
+;; nil
+
+(find 'living-room (mapcar 'car *nodes*))
+;; living-room
+(find 'test (mapcar 'car *nodes*))
+;; nil
+
+(describe-paths *location* *edges*)
+(find 'garden (mapcar #'car (cdr (assoc *location* *edges*))))
+(find 'attic (mapcar #'car (cdr (assoc *location* *edges*))))
+
+(defun walk (direction)
+  (if (find direction (mapcar 'car *nodes*))
+      ;; direction ok, can we go there?
+      (if (find direction (mapcar #'car (cdr (assoc *location* *edges*))))
+          ;; all is ok, the player can go
+          (progn (setf *location* direction)
+                 (look))
+          '(this direction is not possible from here.))
+      '(this direction is unknown.)))
+
+(walk 'no-where)
+;; '(this direction is unknown)
+
+(setf *location* 'attic)
+(walk 'garden)
+;; '(this direction is not possible from here.)
+
+(setf *location* 'living-room)
+(walk 'attic)
+;; (you are in the attic. there is a giant welding torch in the corner. there is a ladder going downstairs from here.)
+*location*
+;; 'attic
+(walk 'living-room)
+ ;; (you are in the living-room. a wizard is snoring loudly on the couch. there is a door going west from here. there is a ladder going upstairs from here. you see a
+ ;; whiskey on the floor. you see a bucket on the floor.)
+*location*
+;; 'living-room
+(walk 'garden)
+;; '(you are in a beautiful garden. there is a well in front of you. there is a door going east from here. you see a frog on the floor. you see a chain on the floor.)

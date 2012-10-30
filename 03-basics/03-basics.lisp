56fc21497a0cfcc9f196160ad80a3a7689362d69
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-10-30 17:45:20 +0100

    some more example with mapcar

--- 03-basics/03-basics.lisp
+++ 03-basics/03-basics.lisp
@@ -267,6 +267,12 @@ nil
 
 ;; #' is a macro that expands into the (function form)
 
+(mapcar #'car '((10 20 30) (40 50) ))
+;; '(10 40)
+
+(apply #'append (mapcar #'cdr ((10 20 30) (40 50))))
+;; '(20 30 50)
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; append
 
 (append '(1 3 2) '(:a :b))

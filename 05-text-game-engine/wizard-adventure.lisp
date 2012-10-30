cd4643ad8d763920234a6c63eec3dad13e37e83e
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-10-30 09:43:51 +0100

    describe-paths - mapcar, apply, append

--- 05-text-game-engine/wizard-adventure.lisp
+++ 05-text-game-engine/wizard-adventure.lisp
@@ -29,8 +29,8 @@
 
 ;; the paths the player can take from one place (one node) to another (node)
 (defparameter *edges*
-  '((living-room ((garden west door)
-                  (attic upstairs ladder)))
+  '((living-room (garden west door)
+                 (attic upstairs ladder))
     (garden      (living-room east door))
     (attic       (living-room downstairs ladder))))
 
@@ -50,4 +50,29 @@
 (describe-path '(garden west door))
 ;; '(there is a door going west from here.)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; describe-paths
+
+(mapcar #'describe-path '((garden west door) (attic upstairs ladder)))
+;; '((there is a door going west from here.) (there is a ladder going upstairs from here.))
+
+(cdr (assoc 'living-room *edges*))
+;; '((garden west door) (attic upstairs ladder))
+
+(mapcar #'describe-path (cdr (assoc 'living-room *edges*)))
+;; '((there is a door going west from here.) (there is a ladder going upstairs from here.))
+
+(apply #'append (mapcar #'describe-path (cdr (assoc 'living-room *edges*))))
+;; '(there is a door going west from here. there is a ladder going upstairs from here.)
+
+(defun describe-paths (location edges)
+  (apply #'append
+         (mapcar #'describe-path (cdr (assoc location edges)))))
+
+(describe-paths 'living-room *edges*)
+;; '(there is a door going west from here. there is a ladder going upstairs from here.)
+
+(describe-paths 'garden *edges*)
+;; '(there is a door going east from here)
+(describe-paths 'attic *edges*)
+;; '(there is a ladder going downstairs from here)
 

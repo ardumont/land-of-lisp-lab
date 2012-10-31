09b0384294c7df424d846f0a274be2c3fdec0bfb
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-10-31 20:17:48 +0100

    More advanced repl with limited functions possibles.

--- 03-basics/06-print-read/game-repl.lisp
+++ 03-basics/06-print-read/game-repl.lisp
@@ -1,7 +1,12 @@
+(defparameter *available-cmds* '(pickup look walk inventory quit))
+
 (defun game-repl ()
   (progn
     (princ "wizard> ")
     (let ((cmd (read)))
       (unless (eq cmd 'quit)
-        (print (eval cmd))
-        (game-repl)))))
+        (if (member (car cmd) *available-cmds*)
+            (progn
+              (print (eval cmd))
+              (game-repl))
+          `(command ,cmd unknown!))))))

76e3779e65e794d7c9e1da22ff9c3d0ee605dd13
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-11-01 11:35:04 +0100

    my game-print implementation

--- 06-print-read/game-repl.lisp
+++ 06-print-read/game-repl.lisp
@@ -12,11 +12,27 @@
       (eval cmd)
       `(command ,cmd unknown!)))
 
+(defun game-print (msg)
+  (flet ((interpose (c l)
+                    (cons (car l)
+                          (reduce (lambda (a e) (cons c (cons e a)))
+                                  (reverse (cdr l))
+                                  :initial-value ())))
+         (as-str (sym)
+                 (string-downcase (symbol-name sym))))
+    (apply #'concatenate 'string
+           (interpose " " (mapcar #'as-str msg)))))
+
+(game-print '(this message will display as is but as a string.))
+;; this message will display as is but as a string.
+(game-print '(this message will display as is but as a string!?))
+;; this message will display as is but as a string!?
+
 (defun game-repl ()
   (progn
     (princ "wizard> ")
     (let ((cmd (game-read)))
       (unless (eq (car cmd) 'quit)
         (progn
-          (print (game-eval cmd))
+          (princ (game-print (game-eval cmd)))
           (game-repl))))))

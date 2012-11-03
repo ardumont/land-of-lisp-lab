b97cb8af6abc50889d6b66a2a73b88d2b59e9e9f
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-11-03 17:02:26 +0100

    dot-label - write-to-string, subseq

--- 07-graphviz/graph.lisp
+++ 07-graphviz/graph.lisp
@@ -17,9 +17,35 @@
 (prin1-to-string "some-name")
 
 ;; compute a graphviz's dot format compliant name
-(defun dot-name (name)
-  (substitute-if #\_ (complement #'alphanumericp) (prin1-to-string name)))
+(defun dot-name (exp)
+  (substitute-if #\_ (complement #'alphanumericp) (prin1-to-string exp)))
 
 (dot-name 'this_is_some_test!)
 (dot-name 'this-is-some_test!)
 ;; "THIS_IS_SOME_TEST_"
+
+(defparameter *max-length* 30)
+
+(write-to-string 'this-is-way-too-much-in-regards-of-the-30-characters-limit
+                 :pretty nil)
+
+;; compute the label (and limit to 30 characters)
+(defun dot-label (exp)
+  (if exp
+      (let ((s (write-to-string exp :pretty nil)))
+        (if (< *max-length* (length s))
+            ;; +
+            (concatenate 'string (subseq s 0 (- *max-length* 3)) "...")
+          s))
+    ""))
+
+(dot-label nil)
+;; ""
+
+(dot-label 'this-is-ok)
+;; "THIS-IS-OK"
+
+(dot-label 'this-is-way-too-much-in-regards-of-the-30-characters-limit)
+;; "THIS-IS-WAY-TOO-MUCH-IN-REG..."
+
+
